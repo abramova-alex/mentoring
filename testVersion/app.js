8e63bfc1617411213d6447f4f92848b9b61b01e8
Oleksandra Abramova <Oleksandra_Abramova@epam.com>
2016-02-03 17:06:36 +0200

    restructure code

--- testVersion/app.js
+++ testVersion/app.js
@@ -1,30 +1,34 @@
-var homeModule = sfMvc.createModule();
+(function () {
+    var homeModule = sfMvc.createModule();
 
-homeModule
-    .modelCreate({Message :'Hello World'})
-    .viewCreate('Views/home.html')
-    .controllerCreate(function (view, model) { console.log("home page")});
+    homeModule
+        .modelCreate({Message: 'Hello World'})
+        .viewCreate('Views/home.html')
+        .controllerCreate(function (view, model) {
+            console.log("home page");
+        });
 
-var contModule = sfMvc.createModule();
+    var contModule = sfMvc.createModule();
 
-contModule
-    .modelCreate({FirstName: "Sasha", LastName: "Abramova", Address: 'Kiev' })
-    .viewCreate('Views/contact.html')
-    .controllerCreate(function (view, model) {
-        console.log("Contact page");
-    });
+    contModule
+        .modelCreate({FirstName: "Sasha", LastName: "Abramova", Address: 'Kiev'})
+        .viewCreate('Views/contact.html')
+        .controllerCreate(function (view, model) {
+            console.log("Contact page");
+        });
 
-var loginModule = sfMvc.createModule();
+    var loginModule = sfMvc.createModule();
 
-loginModule
-    .modelCreate({UserName : "Sasha", Password : "qwerty"})
-    .viewCreate('Views/login.html')
-    .controllerCreate(function (view, model) {
-        console.log("login page");
-    });
+    loginModule
+        .modelCreate({UserName: "Sasha", Password: "qwerty"})
+        .viewCreate('Views/login.html')
+        .controllerCreate(function (view, model) {
+            console.log("login page");
+        });
 
+    sfMvc.AddRoute(homeModule, 'home');
+    sfMvc.AddRoute(contModule, 'contact');
+    sfMvc.AddRoute(loginModule, 'login');
+    sfMvc.init();
 
-sfMvc.AddRoute(homeModule, 'home');
-sfMvc.AddRoute(contModule, 'contact');
-sfMvc.AddRoute(loginModule, 'login');
-sfMvc.init();
+})();
